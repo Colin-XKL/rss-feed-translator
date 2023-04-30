ea0227dfe3fcbc78bbba8fb96ad996338fa7dc14
colin <Colin_XKL@outlook.com>
2023-04-30 23:45:07 +0800

    feat: add feed translation demo

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
-import logging
-import pprint
-import xml.parsers.expat
+from __future__ import annotations
 
+import logging
+import xml.parsers.expat
+from typing import Callable
+
+import requests
 import xmltodict
-from flask import Flask
+from flask import Flask, request, Response
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
@@ -22,10 +25,25 @@ def translate_test():
     return translated
 
 
+@app.route('/translate', methods=['GET'])
+def fetch_and_translate_feed():
+    logging.info("fetch remote feed")
+    feed_url = request.args.get('feed_url')
+    resp = requests.get(feed_url)
+    # print(resp.content)
+    xml_content = resp.content
+
+    # pprint.pprint(d)
+    ret = translate_feed(xml_content)
+
+    return Response(xmltodict.unparse(ret), mimetype='text/xml')
+
+
 @app.route('/feed_parse_test')
 def feed_parse_test():
     logging.info("feed parse test hit.")
-    test_xml_fp = '/Users/colin/Downloads/tttt/rss20.xml'
+    # test_xml_fp = '/Users/colin/Downloads/tttt/rss20.xml'
+    test_xml_fp = '/Users/colin/Downloads/tttt/feed.xml'
     with open(test_xml_fp, 'r') as f:
         xml_str = f.read()
         ret = translate_feed(xml_str)
@@ -33,31 +51,147 @@ def feed_parse_test():
     return ret
 
 
-def translate_feed(xml_str: str) -> str:
+def check_is_array(item) -> bool:
+    return hasattr(item, "__len__") and type(item) is not str and type(item) is not dict
+
+
+def check_is_dict(item) -> bool:
+    return type(item) is dict
+
+
+def recursive_find_node_and_apply(target: dict, field: str, func: Callable):
+    if check_is_dict(target):
+        for k in target:
+            if target[k] and k == field:
+                if check_is_array(target[k]):
+                    logging.warning("unexpected type, make sure the field always point to a leaf node.")
+                elif check_is_dict(target[k]):
+                    if target[k]['#text']:
+                        # special handling for xml #text attr
+                        target[k]['#text'] = func(target[k]['#text'])
+                    else:
+                        logging.warning("unexpected structure.")
+                        logging.warning(target[k])
+                else:
+                    target[k] = func(target[k])
+                continue
+
+            target[k] = recursive_find_node_and_apply(target[k], field, func)
+    elif check_is_array(target):
+
+        for index, item in enumerate(target):
+            target[index] = recursive_find_node_and_apply(item, field, func)
+    else:  # is str or something like that
+        # target = func(target)
+
+        return target
+    return target
+
+
+def check_and_manipulate(val):
+    try:
+        if check_is_dict(val) and val.get("#text", None):
+            pass
+        elif check_if_only_has_link(val):
+            return val
+        else:
+            # return maniputate_func(val)
+            return translate_html(val)
+    except xml.parsers.expat.ExpatError:
+        pass
+    except TypeError:
+        logging.warning("type error")
+        # logging.warning(val)
+        logging.warning(type(val))
+
+    return translate_html(val)
+
+
+def translate_feed(xml_str: str | bytes) -> dict:
     parsed = xmltodict.parse(xml_str)
     # pprint.pprint(parsed)
-    if parsed.get('rss', None):  # rss
-        parsed['rss']['channel']['title'] = "Hello Feed"
-        N = parsed['rss']['channel']['item']
+    if parsed.get('rss', None) or parsed.get('feed', None):  # rss or atom
+        # if parsed.get('rss', None):  # rss
+        # logging.info("rss feed")
+        # pprint.pprint(parsed)
 
-        if hasattr(N, '__len__') and (not isinstance(N, str)):
-            for item in N:
-                # pprint.pprint(item)
+        field_to_translate = ["title", "content", "description", "summary"]
+        ret = parsed
+        for field in field_to_translate:
+            ret = recursive_find_node_and_apply(ret, field, check_and_manipulate)
 
-                item['title'] = translate_html(item['title'])
-                try:
-                    if check_if_only_has_link(item['description']):
-                        # ignore for only link
-                        continue
-                    else:
-                        item['description'] = translate_html(item['description'])
-                except xml.parsers.expat.ExpatError:
-                    pass
+        #
+        # from jsonpath_ng import jsonpath, parse
+        #
+        # # A robust parser, not just a regex. (Makes powerful extensions possible; see below)
+        # jsonpath_expr = parse('foo[*].baz')
+        #
+        #
 
-        ret = xmltodict.unparse(parsed)
-    else:  # else atom feed etc.
+        # # parsed['rss']['channel']['title'] = "Hello Feed"
+        # N = parsed['rss']['channel']['item']
+        #
+        # if hasattr(N, '__len__') and (not isinstance(N, str)):  # array
+        #     for item in N:
+        #         # pprint.pprint(item)
+        #
+        #         item['title'] = translate_html(item['title'])
+        #         if item['content']:
+        #             item['content'] = check_and_translate(item, 'content')
+        #         if item['description']:
+        #             item['description'] = check_and_translate(item, 'description')
+        # else:  # single item
+        #     item = parsed['rss']['channel']['item']
+        #     item['title'] = translate_html(item['title'])
+        #     if item['content']:
+        #         item['content'] = check_and_translate(item, 'content')
+        #     if item['description']:
+        #         item['description'] = check_and_translate(item, 'description')
+        #
+        # ret = xmltodict.unparse(parsed)
+
+    # elif parsed.get('feed', None):  # atom
+    # logging.info("atom feed")
+    # entry_list = parsed['feed']['entry']
+    # if hasattr(entry_list, '__len__') and (not isinstance(entry_list, str)):  # array
+    #     for item in entry_list:
+    #         item['title'] = translate_html(item['title'])
+    #         if item['content']:
+    #             item['content'] = check_and_translate(item, 'content')
+    #         item['description'] = check_and_translate(item, 'description')
+    # else:
+    #     item = parsed['feed']['entry']
+    #     item['title'] = translate_html(item['title'])
+    #     if item['content']:
+    #         item['content'] = check_and_translate(item, 'content')
+    #     if item['description']:
+    #         item['description'] = check_and_translate(item, 'description')
+    # ret = xmltodict.unparse(parsed)
+    else:  # else
         ret = "Unimplemented yet"
     return ret
+    # return
+
+
+# def check_and_translate(item, key):
+#     if key in item:
+#         try:
+#             if check_if_only_has_link(item[key]):
+#                 # ignore for only link
+#                 pass
+#             else:
+#                 return translate_html(item[key])
+#         except xml.parsers.expat.ExpatError:
+#             pass
+#         except TypeError:
+#
+#             logging.warning("type error")
+#             # logging.warning(item[key])
+#             pprint.pprint(item)
+#
+#         return item[key]
+#     else:
+#         return None
 
 
 def check_if_only_has_link(input_xml: str) -> bool:
@@ -66,7 +200,9 @@ def check_if_only_has_link(input_xml: str) -> bool:
     return len(item) == 0
 
 
-def translate_html(input_html, translator_name="deepl", from_lang="auto", target_lang="Chinese"):
+# TODO more translation backend
+# maybe paddle nlp
+def translate_html(input_html, translator_name="", from_lang="auto", target_lang="Chinese"):
     logging.info(f"translating using [{translator_name}] from [{from_lang}] to [{target_lang}]...")
 
     if translator_name == "deepl":
@@ -83,4 +219,4 @@ def translate_html(input_html, translator_name="deepl", from_lang="auto", target
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
